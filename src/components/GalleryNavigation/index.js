6353f891503142fe6f2f34f346381b7427569ba3
Alvin Ly <alvinly74@gmail.com>
2024-03-26 22:18:52 -0700

    initial commit and day 1 activity

---
+++ src/components/GalleryNavigation/index.js
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom'
+import './GalleryNavigation.css'
+
+function GalleryNavigation(props) {
+    const records = props.records;
+    return (
+            <nav>
+            <h1>Hello from GalleryNavigation</h1>
+                <NavLink key='home' exact to={`/`}>
+                    Home
+                </NavLink>'
+                {records.map((record) => (
+                    <NavLink key={record.id} to={`/galleries/${record.id}`}>
+                        {record.name}
+                    </NavLink>
+                    ))}
+            </nav>
+    );
+  }
+  export default GalleryNavigation
